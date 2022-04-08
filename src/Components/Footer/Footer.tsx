85d8b9e7e7549db81b74cdaeaeb62ec64f34c2c1
jessie <creators.nuke@gmail.com>
2022-04-08 12:15:54 +0800

    fixed: footer grammar

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className={styles.footer} id="contact">
       <div className={styles.footer_container}>
-        <span className={styles.footer_span}>Design and Develop by Jessie Tarrosa</span>
+        <span className={styles.footer_span}>Designed and Developed by Jessie Tarrosa</span>
         <span className={styles.footer_span}>@{year} JessieT Dev</span>
       </div>
     </footer>
