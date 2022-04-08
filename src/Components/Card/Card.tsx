9e40a20e25ed40a37204e099806b9d9a6ebe628a
jessie <creators.nuke@gmail.com>
2022-04-09 07:01:08 +0800

    feat: lazy image blur effect

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import styles from './Card.module.scss';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 interface CardProps {
   name: string;
@@ -11,7 +12,7 @@ export const Card: FC<CardProps> = ({ name, src }) => {
   return (
     <div className={styles.card}>
       <div className={styles.image_container}>
-        <LazyLoadImage src={src} alt={name} effect="opacity" />
+        <LazyLoadImage src={src} alt={name} effect="blur" />
       </div>
       <h1 className={styles.card_name}>{name}</h1>
     </div>
