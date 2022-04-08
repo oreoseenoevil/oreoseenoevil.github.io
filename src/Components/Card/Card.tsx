9050faa0572c713894fee0b5b7b273271facaf2d
jessie <creators.nuke@gmail.com>
2022-04-08 15:19:25 +0800

    feat: lazyload opacity

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,6 +1,5 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { SkeletonLoader } from 'Components/SkeletonLoader';
 import styles from './Card.module.scss';
 
 interface CardProps {
@@ -9,22 +8,10 @@ interface CardProps {
 }
 
 export const Card: FC<CardProps> = ({ name, src }) => {
-  const [onLoad, setOnLoad] = useState(true);
-
   return (
     <div className={styles.card}>
       <div className={styles.image_container}>
-        {onLoad && (
-          <SkeletonLoader>
-            <rect x="0" y="0" rx="0" ry="0" width="100%" height="100%" />
-          </SkeletonLoader>
-        )}
-        <LazyLoadImage
-          src={src}
-          alt={name}
-          afterLoad={() => setOnLoad(false)}
-          style={onLoad ? { display: 'none' } : {}}
-        />
+        <LazyLoadImage src={src} alt={name} effect="opacity" />
       </div>
       <h1 className={styles.card_name}>{name}</h1>
     </div>
