4438dc230e34172f4a83c01507631e2bf27f2604
jessie <creators.nuke@gmail.com>
2022-04-08 11:55:02 +0800

    revert: load fallback ui

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { SkeletonLoader } from 'Components/SkeletonLoader';
 import styles from './Card.module.scss';
 
 interface CardProps {
@@ -8,10 +9,17 @@ interface CardProps {
 }
 
 export const Card: FC<CardProps> = ({ name, src }) => {
+  const [onLoad, setOnLoad] = useState(true);
+
   return (
     <div className={styles.card}>
       <div className={styles.image_container}>
-        <LazyLoadImage src={src} alt={name} />
+        {onLoad && (
+          <SkeletonLoader>
+            <rect x="0" y="0" rx="0" ry="0" width="100%" height="100%" />
+          </SkeletonLoader>
+        )}
+        <LazyLoadImage src={src} alt={name} afterLoad={() => setOnLoad(false)} />
       </div>
       <h1 className={styles.card_name}>{name}</h1>
     </div>
